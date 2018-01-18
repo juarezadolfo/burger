a1e247f6cacbf369063c5be5afb0f96aae182991
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-18 00:15:35 -0800

    added DB and JS

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Blue Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki Pineapple Burger', false);
+
+SELECT*FROM burgers;
